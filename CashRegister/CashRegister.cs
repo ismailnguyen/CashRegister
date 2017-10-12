45d085f0f5b34584cc493688587f84952fa0c77b
ismailnguyen <nguyen.ismail@gmail.com>
2017-10-12 15:36:18 +0200

    Calcul total between double price and double quantity

---
+++ CashRegister/CashRegister.cs
@@ -0,0 +1,10 @@
+﻿namespace KataCashRegister
+{
+    public class CashRegister
+    {
+        public double Total(double price, double quantity)
+        {
+            return price * quantity;
+        }
+    }
+}
---
+++ CashRegisterTest/CashRegisterTest.cs
@@ -0,0 +1,22 @@
+﻿using KataCashRegister;
+using NFluent;
+using NUnit.Framework;
+
+namespace KataCashRegisterTest
+{
+    public class CashRegisterTest
+    {
+        [Test]
+        public void Total_Should_Calcul_Total_Price()
+        {
+            var cashRegister = new CashRegister();
+
+            double price = 1.20;
+            double quantity = 1;
+
+            double total = cashRegister.Total(price, quantity);
+
+            Check.That(total).IsEqualTo(1.20);
+        }
+    }
+}
