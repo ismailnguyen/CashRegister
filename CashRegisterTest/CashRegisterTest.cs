06b2864f54a688d5d4a16122839434f0791d48c6
ismailnguyen <nguyen.ismail@gmail.com>
2017-10-12 18:11:53 +0200

    Use setup method for test to initialize parameters

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -5,6 +5,17 @@ namespace KataCashRegister
 {
     public class CashRegisterTest
     {
+        PriceQuery PriceQuery;
+
+        [SetUp]
+        public void init()
+        {
+            PriceQuery = new PriceQuery(
+                ItemReference.AReference().WithItemCode("APPLE").WithUnitPrice(1.20).Build(),
+                ItemReference.AReference().WithItemCode("BANANA").WithUnitPrice(1.90).Build()
+                );
+        }
+
         [Test]
         public void Total_Should_Calcul_Total_Price()
         {
@@ -22,23 +33,13 @@ namespace KataCashRegister
         [TestCase("BANANA", 1.90)]
         public void Find_The_Price_Given_An_Item_Code(string itemCode, double unitPrice)
         {
-            PriceQuery priceQuery = new PriceQuery(
-                ItemReference.AReference().WithItemCode("APPLE").WithUnitPrice(1.20).Build(),
-                ItemReference.AReference().WithItemCode("BANANA").WithUnitPrice(1.90).Build()
-                );
-
-            Check.That(priceQuery.FindPrice(itemCode)).IsEqualTo(Price.ValueOf(unitPrice));
+            Check.That(PriceQuery.FindPrice(itemCode)).IsEqualTo(Price.ValueOf(unitPrice));
         }
 
         [Test]
         public void Search_An_Unknown_Item()
         {
-            PriceQuery priceQuery = new PriceQuery(
-               ItemReference.AReference().WithItemCode("APPLE").WithUnitPrice(1.20).Build(),
-               ItemReference.AReference().WithItemCode("BANANA").WithUnitPrice(1.90).Build()
-               );
-
-            Check.That(priceQuery.FindPrice("PEACH")).IsNull();
+            Check.That(PriceQuery.FindPrice("PEACH")).IsNull();
         }
     }
 }
